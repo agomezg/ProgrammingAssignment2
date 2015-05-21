e768acd6af4701795414d4c4992abe2199cd1ce7
agomezg <alex.gomez.garrido@gmail.com>
2015-05-22 01:28:01 +0200

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Alex Gómez Garrido
+## 22/05/2015
+## Coursera R programming: Programming Assignment 2
 
-## Write a short comment describing this function
+## This module is able to create a special matrix object,
+## where the matrix is sotred, but also its inverse can be calculated and stored.
+
+## makeCacheMatrix: Argument should be a Matrix
+## This function creates this special matrix object.
+## The methods implemented in this object are:
+## set: Store the matrix.
+## get: Return the matrix.
+## setInverse: Set the inverse matrix variable with a matrix (not necessary the inverse matrix)
+## getInverse: Return the matrix stored in the inverse matrix variable
 
 makeCacheMatrix <- function(x = matrix()) {
   if(!is.matrix(x)){
@@ -18,8 +28,12 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
+## CacheSolve: Argument should be a Matrix
+## This function does the inverse of a quadratic matrix with determinant different than zero.
+## If the matrix is the special matrix object created in this module it first look
+## if the inverse matrix is already stored in the cahce of this object and return this,
+## but if it is not there, it calculates and return the inverse matrix.
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
